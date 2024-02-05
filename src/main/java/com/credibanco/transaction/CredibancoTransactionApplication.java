0077502a04ec4affeb53cd082291384850072f25
carlosdev29 <caluss48@hotmail.com>
2024-02-04 23:10:36 -0500

    Se actualiza repo con api para las transacciones

---
+++ src/main/java/com/credibanco/transaction/CredibancoTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.credibanco.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredibancoTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredibancoTransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/credibanco/transaction/CredibancoTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credibanco.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredibancoTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
